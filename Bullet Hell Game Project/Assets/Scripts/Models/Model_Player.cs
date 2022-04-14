e7793e156b7ee072f0c29cf1bda1556bba344108
Max Swietnicki <swietnickimax@gmail.com>
2022-04-13 20:08:55 -0400

    newstuff

--- Bullet Hell Game Project/Assets/Scripts/Models/Model_Player.cs
+++ Bullet Hell Game Project/Assets/Scripts/Models/Model_Player.cs
@@ -26,20 +26,22 @@ public class Model_Player : MonoBehaviour
     public int bulletGrazes;
     public int grazesForPowerup;
     public float grazePowerupInterval;
+    public float shiftTurningFactor;
 
     [Header("Tuning Values")]
     [Range(0.01f, 30.0f)] public float smoothingFactor;
-    public float shipSpeed = 12f;
-    public float bulletSpeed = 2f;
-    public float fireRate = 2f;
-    public float limitHorz = 10;
-    public float limitVert = 5;
-    public float shieldRegenIntervalBase = 1.25f;
-    public float shieldDurationBase = 3f;
-    public float turnLimit = 45f;
-    public float rotationSpeed = 2f;
-    public float vFactor = 0.25f;
-    public float shieldCooldownBase = 10f;
+
+    public float shipSpeed;
+    public float bulletSpeed;
+    public float fireRate;
+    public float limitHorz;
+    public float limitVert;
+    public float shieldRegenIntervalBase;
+    public float shieldDurationBase;
+    public float turnLimit;
+    public float rotationSpeed;
+    public float vFactor;
+    public float shieldCooldownBase;
 
     [Header("Controlled Variables")]
     public Vector3 positionTarget;
