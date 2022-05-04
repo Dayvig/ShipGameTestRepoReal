5c43e3d800a3a2bf16e561aba5d9ff3619e6c126
SDAxium <sdaxium@gmail.com>
2022-05-04 18:51:03 -0400

    Dialogue
    Created basic dialogue functionality and added stills to dialogue scene

---
+++ Bullet Hell Game Project/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+    private int index;
+    
+    void Start()
+    {
+        textComponent.text = String.Empty;
+        StartDialogue();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = String.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+        }
+    }
+   
+}
